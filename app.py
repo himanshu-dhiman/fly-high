1b692f97d32b396f6c34a509b25b40ea53c6f5ca
Himanshu Dhiman <himanshu.dhiman@decentro.tech>
2022-09-11 02:22:58 +0530

    Added CORS resolver

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from src.resource.plan import Plan
 
 app = Flask(__name__)
+
+CORS(app)
 
 api = Api(app)
 
